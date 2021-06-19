59b33ece13b1348df95b0ca0636a892c53641693
JCCKwong <62458997+JCCKwong@users.noreply.github.com>
2021-06-19 19:58:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -53,13 +53,21 @@ def full_app(session_state):
 
     CKD_model = load_items()
 
+    # Define choices and labels for feature inputs
+    CHOICES = {0: 'No', 1: 'Yes'}
+
+    def format_func_yn(option):
+        return CHOICES[option]
+
     with st.sidebar:
         with st.form(key='my_form'):
             egfr = st.number_input('Baseline eGFR', 0, 1000, value=60, key=1)
-            oligohydramnios = st.selectbox('Antenatal oligohydramnios', options=list({0: 'No', 1: 'Yes'}.keys()), index=0)
-            renal_dysplasia = st.selectbox('Antenatal/Postnatal renal dysplasia', options=list({0: 'No', 1: 'Yes'}.keys()),
-                                       index=0)
-            vur = st.selectbox('High Grade VUR on initial VCUG (Grade 4-5)', options=list({0: 'No', 1: 'Yes'}.keys()), index=0)
+            oligohydramnios = st.selectbox('Antenatal oligohydramnios', options=list(CHOICES.keys()),
+                                           format_func=format_func_yn, index=0)
+            renal_dysplasia = st.selectbox('Antenatal/Postnatal renal dysplasia', options=list(CHOICES.keys()),
+                                           format_func=format_func_yn, index=0)
+            vur = st.selectbox('High Grade VUR on initial VCUG (Grade 4-5)', options=list(CHOICES.keys()),
+                               format_func=format_func_yn, index=0)
             submitted = st.form_submit_button(label='Submit')
 
             if submitted:
