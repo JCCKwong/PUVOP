e65b98d2240046d60ca00193ababbb6fd29f948a
JCCKwong <62458997+JCCKwong@users.noreply.github.com>
2021-06-19 19:13:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-import matplotlib as plt
-import joblib
+import matplotlib.pyplot as plt
 from pathlib import Path
 from google_drive_downloader import GoogleDriveDownloader as gdd
 import SessionState
-
+from pysurvival.utils import load_model
 
 def main():
     st.title("Posterior Urethral Valves Outcomes Prediction")
@@ -34,10 +33,26 @@ def full_app(session_state):
     """
     )
 
-    # Load saved items
-    from pysurvival.utils import load_model
-    CKD_model = load_model('PUV CKD survival.zip')
-    
+    # Load saved items from Google Drive
+    CKD_location = st.secrets['CKD']
+
+    @st.cache(allow_output_mutation=True)
+    def load_items():
+        save_dest = Path('model')
+        save_dest.mkdir(exist_ok=True)
+        CKD_checkpoint = Path('model/CKD.zip')
+
+        # download from Google Drive if model or features are not present
+        if not CKD_checkpoint.exists():
+            with st.spinner("Downloading model... this may take awhile! \n Don't stop it!"):
+                gdd.download_file_from_google_drive(CKD_location, CKD_checkpoint)
+
+        CKD_model = load_model(CKD_checkpoint)
+
+        return CKD_model
+
+    CKD_model = load_items()
+
     with st.sidebar:
         with st.form(key='my_form'):
             egfr = st.number_input('Baseline eGFR', 0, 1000, value=60, key=1)
